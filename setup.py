187da6657bd134f5db0af65b9b22ef1fa59972bb
faradox <felix@okio.ai>
2024-02-21 10:44:29 +0100

    chore: prepare release 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-vampnet",
-        version="0.1.2",
+        version="0.1.3",
         description="Nendo VampNet plugin",
         author="Aaron Abebe <aaron@okio.ai>",
     )
